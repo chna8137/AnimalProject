359306dfbe678415ec72d7b9034dac384902e20d
chna8137 <chna8137s@gmail.com>
2024-04-02 18:22:10 +0900

    240402 개인프로젝트 개설

---
+++ src/test/java/kopo/animal/AnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
