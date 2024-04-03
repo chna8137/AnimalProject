b84f03025a14ce936018ce540cac60f627166305
chna8137 <chna8137s@gmail.com>
2024-04-03 16:22:14 +0900

    240403_UserInfoDTO생성

---
+++ src/main/java/kopo/animal/dto/UserInfoDTO.java
@@ -0,0 +1,38 @@
+package kopo.animal.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class UserInfoDTO {
+
+    private String userId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String addr1;
+
+    private String addr2;
+
+    private String regId;
+
+    private String regDt;
+
+    private String chgId;
+
+    private String chgDt;
+
+    // 회원가입시, 중복가입을 방지를 위해 사용할 변수
+    private String existsYn;
+
+    // 이메일 중복체크를 위한 중복체크
+    private String authNumber;
+
+}
