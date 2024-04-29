43812033492c7c08b64f3e9279e9221872f9a35d
chna8137 <chna8137s@gmail.com>
2024-04-29 15:19:54 +0900

    240429_채팅 구현 중

--- src/main/java/kopo/animal/controller/ChatController.java
+++ src/main/java/kopo/animal/controller/ChatController.java
@@ -1,8 +1,11 @@
 package kopo.animal.controller;
 
+import jakarta.servlet.http.HttpSession;
 import kopo.animal.chat.ChatHandler;
+import kopo.animal.util.CmmUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,21 @@ public class ChatController {
      * 채팅창 입장 전
      * */
     @GetMapping(value = "intro")
-    public String intro() {
+    public String intro(HttpSession session, ModelMap model) {
 
         log.info(this.getClass().getName() + "인트로 시작!");
+
+        String userId = CmmUtil.nvl((String) session.getAttribute("SS_USER_ID"));
+
+        if (userId.length() < 1) {
+            String msg = "로그인 정보가 없습니다.\\n로그인후 이용해 주세요.";
+            String url = "/user/login";
+
+            model.addAttribute("msg", msg);
+            model.addAttribute("url", url);
+
+            return "/redirect";
+        }
 
         log.info(this.getClass().getName() + "인트로 종료!");
 
