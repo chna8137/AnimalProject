1501194c60f1925e10cfad666d0d84cee15f6b79
chna8137 <chna8137s@gmail.com>
2024-05-10 13:17:39 +0900

    2405010_채팅 구현 완료

--- src/main/java/kopo/animal/config/WebSocketConfig.java
+++ src/main/java/kopo/animal/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         log.info("WebSocket Execute!!!");
 
-        registry.addHandler(chatHandler, "/ws/*/*/*")
+        registry.addHandler(chatHandler, "/ws/*/*")
                 .setAllowedOrigins("*")
                 .addInterceptors(
                         new HttpSessionHandshakeInterceptor() {
@@ -46,20 +46,19 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
                                 String roomName = CmmUtil.nvl(urlInfo[2]); // URI Path를 통해 채팅방 이름 가져오기
                                 String userName = CmmUtil.nvl(urlInfo[3]); // URI Path를 통해 사용자 이름 가져오기
-                                String langCode = CmmUtil.nvl(urlInfo[4]); // URI Path를 통해 채팅방의 언어 타입 가져오기
+//                                String langCode = CmmUtil.nvl(urlInfo[4]); // URI Path를 통해 채팅방의 언어 타입 가져오기
 
                                 // 채팅룸 이름이 한글 및 특수문자로 입력될 수 있기에
                                 // 채팅룸 이름은 데이터 처리에 문제없는 영문이나 숫자로 변환해야 함
                                 // 채팅룸 이름은 해시 함수를 이용하여 영문명으로 변경함
                                 String roomNameHash = EncryptUtil.encHashSHA256(roomName);
 
-                                log.info("roomName : " + roomName + " / userName : + " + userName +
-                                        " / langCode : " + langCode);
+                                log.info("roomName : " + roomName + " / userName : + " + userName);
 
                                 attributes.put("roomName", roomName);
                                 attributes.put("userName", userName);
                                 attributes.put("roomNameHash", roomNameHash);
-                                attributes.put("langCode", langCode); // 채팅방 언어(한국어, 영어)
+//                                attributes.put("langCode", langCode); // 채팅방 언어(한국어, 영어)
 
                                 return super.beforeHandshake(request, response, wsHandler, attributes);
                             }
--- src/main/java/kopo/animal/controller/MapController.java
+++ src/main/java/kopo/animal/controller/MapController.java
@@ -2,8 +2,13 @@ package kopo.animal.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kopo.animal.controller.response.CommonResponse;
+import kopo.animal.dto.CultureDTO;
+import kopo.animal.dto.HospitalDTO;
 import kopo.animal.dto.ParkDTO;
+import kopo.animal.service.ICultureService;
+import kopo.animal.service.IHospitalService;
 import kopo.animal.service.IParkService;
+import kopo.animal.util.CmmUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -23,27 +28,25 @@ import java.util.Optional;
 public class MapController {
 
     private final IParkService parkService;
+    private final ICultureService cultureService;
+    private final IHospitalService hospitalService;
 
-    @GetMapping(value = "park")
-    public String park() throws Exception {
-
-        log.info(this.getClass().getName() + ".controller 공원정보 시작!");
-
-        log.info(this.getClass().getName() + ".controller 공원정보 종료!");
-
-        return "map/park";
-    }
 
     /**
      * 수집된 공원 리스트 가져오기
      */
     @PostMapping(value = "/v1/getParkInfo")
-    public ResponseEntity getParkInfo(@RequestParam(defaultValue = "1") int page,
+    public ResponseEntity getParkInfo(HttpServletRequest request, @RequestParam(defaultValue = "1") int page,
                                       @RequestParam(defaultValue = "20") int itemsPerPage) throws Exception {
 
         log.info(this.getClass().getName() + ".controller getParkInfo 시작!");
 
-        List<ParkDTO> rList = Optional.ofNullable(parkService.getParkInfo(page, itemsPerPage))
+        double coursSpotLa = Double.parseDouble(CmmUtil.nvl(request.getParameter("coursSpotLa")));
+        double coursSpotLo = Double.parseDouble(CmmUtil.nvl(request.getParameter("coursSpotLo")));
+
+        ParkDTO pDTO = ParkDTO.builder().coursSpotLa(String.valueOf(coursSpotLa)).coursSpotLo(String.valueOf(coursSpotLo)).build();
+
+        List<ParkDTO> rList = Optional.ofNullable(parkService.getParkInfo(page, itemsPerPage, pDTO))
                 .orElseGet(ArrayList::new);
 
         log.info(this.getClass().getName() + ".controller getParkInfo 종료!");
@@ -61,5 +64,58 @@ public class MapController {
 
         return "map/map";
     }
+
+    /**
+     * 수집된 공원 리스트 가져오기
+     */
+    @PostMapping(value = "/v1/getCultureInfo")
+    public ResponseEntity getCultureInfo(HttpServletRequest request, @RequestParam(defaultValue = "1") int page,
+                                      @RequestParam(defaultValue = "20") int itemsPerPage) throws Exception {
+
+        log.info(this.getClass().getName() + ".controller getCultureInfo 시작!");
+
+        double lcLa = Double.parseDouble(CmmUtil.nvl(request.getParameter("lcLa")));
+        double lcLo = Double.parseDouble(CmmUtil.nvl(request.getParameter("lcLo")));
+
+        log.info("lcla : " + lcLa);
+        log.info("lclo : " + lcLo);
+
+        CultureDTO pDTO = CultureDTO.builder().lcLa(String.valueOf(lcLa)).lcLo(String.valueOf(lcLo)).build();
+
+        List<CultureDTO> rList = Optional.ofNullable(cultureService.getCultureInfo(page, itemsPerPage, pDTO))
+                .orElseGet(ArrayList::new);
+
+        log.info(this.getClass().getName() + ".controller getCultureInfo 종료!");
+
+        return ResponseEntity.ok(
+                CommonResponse.of(HttpStatus.OK, HttpStatus.OK.series().name(), rList));
+    }
+
+    /**
+     * 수집된 공원 리스트 가져오기
+     */
+    @PostMapping(value = "/v1/getHospitalInfo")
+    public ResponseEntity getHospitalInfo(HttpServletRequest request, @RequestParam(defaultValue = "1") int page,
+                                         @RequestParam(defaultValue = "20") int itemsPerPage) throws Exception {
+
+        log.info(this.getClass().getName() + ".controller getHospitalInfo 시작!");
+
+        double fcltyLa = Double.parseDouble(CmmUtil.nvl(request.getParameter("fcltyLa")));
+        double fcltyLo = Double.parseDouble(CmmUtil.nvl(request.getParameter("fcltyLo")));
+
+        log.info("fcltyLa : " + fcltyLa);
+        log.info("fcltyLo : " + fcltyLo);
+
+        HospitalDTO pDTO = HospitalDTO.builder().fcltyLa(String.valueOf(fcltyLa)).fcltyLo(String.valueOf(fcltyLo)).build();
+
+        List<HospitalDTO> rList = Optional.ofNullable(hospitalService.getHospitalInfo(page, itemsPerPage, pDTO))
+                .orElseGet(ArrayList::new);
+
+        log.info(this.getClass().getName() + ".controller getHospitalInfo 종료!");
+
+        return ResponseEntity.ok(
+                CommonResponse.of(HttpStatus.OK, HttpStatus.OK.series().name(), rList));
+    }
+
 
 }
