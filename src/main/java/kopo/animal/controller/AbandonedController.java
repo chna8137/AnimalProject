552f1505d7a058eda6131333cee91308f510d6e1
chna8137 <chna8137s@gmail.com>
2024-05-24 23:54:46 +0900

    240524_유기동물 구현 완료

--- src/main/java/kopo/animal/controller/AbandonedController.java
+++ src/main/java/kopo/animal/controller/AbandonedController.java
@@ -1,14 +1,18 @@
 package kopo.animal.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kopo.animal.controller.response.CommonResponse;
 import kopo.animal.dto.AbandonedDTO;
 import kopo.animal.dto.MsgDTO;
 import kopo.animal.service.IAbandonedService;
+import kopo.animal.util.CmmUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -25,7 +29,7 @@ public class AbandonedController {
 
     /**
      * 유기동물 리스트 저장하기
-     * */
+     */
     @ResponseBody
     @GetMapping(value = "collectAnimal")
     public ResponseEntity collectAnimal() throws Exception {
@@ -57,29 +61,78 @@ public class AbandonedController {
 
     /**
      * 유기동물 리스트 가져오기
-     * */
-    @PostMapping(value = "getAbandonedInfo")
-    public ResponseEntity getAbandonedInfo() throws Exception {
+     */
+    @GetMapping(value = "abandonedList")
+    public String abandonedList(ModelMap model, @RequestParam(defaultValue = "1") int page) throws Exception {
 
         log.info(this.getClass().getName() + ".controller 유기동물 정보 가져오기 시작!");
 
         List<AbandonedDTO> rList = Optional.ofNullable(abandonedService.getAbandonedList())
                 .orElseGet(ArrayList::new);
 
+        /**페이징 시작 부분*/
+
+        // 페이지당 보여줄 아이템 개수 정의
+        // 페이지당 보여줄 아이템 개수 정의
+        int itemsPerPage = 9;
+
+// 페이지네이션을 위해 전체 아이템 개수 구하기
+        int totalItems = rList.size();
+
+// 전체 페이지 개수 계산
+        int totalPages = (int) Math.ceil((double) totalItems / itemsPerPage);
+
+// 현재 페이지에 해당하는 아이템들만 선택하여 rList에 할당
+        int fromIndex = (page - 1) * itemsPerPage;
+        int toIndex = Math.min(fromIndex + itemsPerPage, totalItems);
+        rList = rList.subList(fromIndex, toIndex);
+
+        int pageGroupSize = 10;
+        int currentGroup = (int) Math.ceil((double) page / pageGroupSize);
+        int startPage = (currentGroup - 1) * pageGroupSize + 1;
+        int endPage = Math.min(currentGroup * pageGroupSize, totalPages);
+
+        model.addAttribute("rList", rList);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        log.info(this.getClass().getName() + ".페이지 번호 : " + page);
+
+        /**페이징 끝부분*/
+
+        model.addAttribute("rList", rList);
+
+        log.info("rList : " + rList);
+
 
         log.info(this.getClass().getName() + ".controller 유기동물 정보 가져오기 끝!");
 
-        return ResponseEntity.ok(
-                CommonResponse.of(HttpStatus.OK, HttpStatus.OK.series().name(), rList));
+        return "abandoned/abandonedList";
     }
 
-    @GetMapping(value = "abandonedList")
-    public String abandonedList() throws Exception {
+    // 유기견 상세보기
+    // 2024.05.21 ( 로직 변경 { 컬렉션 이름 Controller로 옮기고 idntfyNo를 직접 가져오도록 변경 )
+    @GetMapping(value = "abandonedInfo")
+    public String abandonedInfo(ModelMap model, @RequestParam("idntfyNo") String idntfyNo) throws Exception {
+        log.info(this.getClass().getName() + ".controller 유기동물 상세보기 시작!");
 
-        log.info(this.getClass().getName() + ".controller abandonedList 시작!");
+        log.info("idntfyNo : " + idntfyNo);
 
-        log.info(this.getClass().getName() + ".controller abandonedList 종료!");
+        // MongoDB에 저장된 컬렉션 이름
+        String colNm = "ANIMAL_ABANDONED";
 
-        return "/abandoned/abandonedList";
+        // 서비스 호출을 통해 rDTO를 가져옴
+        AbandonedDTO rDTO = Optional.ofNullable(abandonedService.getAbandoned(colNm, idntfyNo))
+                .orElseGet(() -> AbandonedDTO.builder().build());
+
+        model.addAttribute("rDTO", rDTO);
+
+        log.info(this.getClass().getName() + ".controller 유기동물 상세보기 종료");
+
+        return "abandoned/abandonedInfo";
     }
+
+
 }
--- src/main/java/kopo/animal/dto/AbandonedDTO.java
+++ src/main/java/kopo/animal/dto/AbandonedDTO.java
@@ -6,62 +6,62 @@ import lombok.Getter;
 
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @Builder
-public record AbandonedDTO (
-      String idntfyNo, // 유기 고유 번호
+public record AbandonedDTO(
+        String idntfyNo, // 유기 고유 번호
 
-      String receptDe, // 접수 일자
+        String receptDe, // 접수 일자
 
-      String plcInfo, // 발견 장소
+        String plcInfo, // 발견 장소
 
-      String state, // 상태
+        String state, // 상태
 
-      String pblancIdntfuNo, // 공고 고유 번호
+        String pblancIdntfuNo, // 공고 고유 번호
 
-      String pblancBeginDe, // 공고 시작 일자
+        String pblancBeginDe, // 공고 시작 일자
 
-      String pblancEndDe, // 공조 종료 일자
+        String pblancEndDe, // 공조 종료 일자
 
-      String species, // 품종
+        String species, // 품종
 
-      String color, // 색상
+        String color, // 색상
 
-      String age, // 나이
+        String age, // 나이
 
-      String weight, // 체중
+        String weight, // 체중
 
-      String sex, // 성별
+        String sex, // 성별
 
-      String neut, // 중성화 여부
+        String neut, // 중성화 여부
 
-      String sfetrInfo, // 특징
+        String sfetrInfo, // 특징
 
-      String shterNm, // 보호소명
+        String shterNm, // 보호소명
 
-      String shterTelno, // 보호소 전화번호
+        String shterTelno, // 보호소 전화번호
 
-      String protectPlc, // 보호장소
+        String protectPlc, // 보호장소
 
-      String refineRoadnmAddr, // 보호소 도로명 주소
+        String refineRoadnmAddr, // 보호소 도로명 주소
 
-      String refineLotnoAddr, // 보호소 지번주소
+        String refineLotnoAddr, // 보호소 지번주소
 
-      String refineZipCd, // 보호소 우편번호
+        String refineZipCd, // 보호소 우편번호
 
-      String jurisdInstNm, // 관할기관
+        String jurisdInstNm, // 관할기관
 
-      String chrgpsnNm, // 담당자
+        String chrgpsnNm, // 담당자
 
-      String chrgpsnContctNo,// 담당자 연락처
+        String chrgpsnContctNo,// 담당자 연락처
 
-      String partclrMatr, // 특이사항
+        String partclrMatr, // 특이사항
 
-      String imageCours, // 이미지 경로
+        String imageCours, // 이미지 경로
 
-      String thumbImageCours, // 썸네일 이미지 경로
+        String thumbImageCours, // 썸네일 이미지 경로
 
-      String lat,// 위도
+        String lat,// 위도
 
-      String lon // 경도
-        
-){
+        String lon // 경도
+
+) {
 }
