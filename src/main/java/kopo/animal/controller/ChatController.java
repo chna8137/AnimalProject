451ee1afbca5c470b485a7ad6973c04678eb967e
chna8137 <chna8137s@gmail.com>
2024-05-26 23:29:20 +0900

    240526_채팅 디자인 완료

--- src/main/java/kopo/animal/controller/ChatController.java
+++ src/main/java/kopo/animal/controller/ChatController.java
@@ -23,21 +23,17 @@ import java.util.Set;
 public class ChatController {
 
     private final IUserInfoService userInfoService;
-
     private final IUserInfoMapper userInfoMapper;
 
-    /**
-     * 채팅창 입장 전
-     * */
     @GetMapping(value = "intro")
     public String intro(HttpSession session, ModelMap model) {
 
-        log.info(this.getClass().getName() + "인트로 시작!");
+        log.info(this.getClass().getName() + " 인트로 시작!");
 
         String userId = CmmUtil.nvl((String) session.getAttribute("SS_USER_ID"));
 
         if (userId.length() < 1) {
-            String msg = "로그인 정보가 없습니다. \n 로그인후 이용해 주세요.";
+            String msg = "로그인 정보가 없습니다. \n 로그인 후 이용해 주세요.";
             String url = "/user/login";
 
             model.addAttribute("msg", msg);
@@ -46,53 +42,51 @@ public class ChatController {
             return "redirect";
         }
 
-        log.info(this.getClass().getName() + "인트로 종료!");
+        log.info(this.getClass().getName() + " 인트로 종료!");
 
         return "chat/intro";
     }
 
-    /**
-     * 채팅방 접속
-     * */
-    @PostMapping(value = "chatroom")
-    public String chatroom(HttpServletRequest request, HttpSession session, ModelMap model) throws Exception {
+    @GetMapping(value = "chatroom")
+    public String chatroom(@RequestParam String roomName, HttpSession session, ModelMap model) throws Exception {
 
-        log.info(this.getClass().getName() + "채팅방 화면 시작!");
+        log.info(this.getClass().getName() + " 채팅방 화면 시작!");
 
-        String roomName = CmmUtil.nvl(request.getParameter("roomName")); // 채팅방 명
-        String nickname = CmmUtil.nvl(request.getParameter("nickname")); // 채팅방 입장전 입력한 별명
         String userId = CmmUtil.nvl((String) session.getAttribute("SS_USER_ID"));
 
         log.info("roomName : " + roomName);
-        log.info("nickname : " + nickname);
         log.info("userId : " + userId);
 
         UserInfoDTO pDTO = new UserInfoDTO();
-        pDTO.setNickname(nickname);
         pDTO.setUserId(userId);
 
         UserInfoDTO rDTO = Optional.ofNullable(userInfoService.getNickname(pDTO)).orElseGet(UserInfoDTO::new);
 
-        model.addAttribute("rDTO", rDTO);
+        String nickname = rDTO.getNickname(); // 세션의 닉네임 정보 사용
+
         model.addAttribute("nickname", nickname);
         model.addAttribute("roomName", roomName);
 
-        log.info(this.getClass().getName() + "채팅방 화면 종료!");
+        log.info(this.getClass().getName() + " 채팅방 화면 종료!");
 
         return "chat/chatroom";
     }
 
-    /**
-     * 채팅방 목록
-     * */
     @RequestMapping(value = "roomList")
     @ResponseBody
     public Set<String> roomList() {
 
-        log.info(this.getClass().getName() + "채팅방 리스트 시작!");
+        log.info(this.getClass().getName() + " 채팅방 리스트 시작!");
 
-        log.info(this.getClass().getName() + "채팅방 리스트 종료!");
+        log.info(this.getClass().getName() + " 채팅방 리스트 종료!");
 
         return ChatHandler.roomInfo.keySet();
+    }
+
+    @PostMapping(value = "createRoom")
+    @ResponseBody
+    public String createRoom(@RequestParam String roomName, HttpSession session) {
+        ChatHandler.roomInfo.putIfAbsent(roomName, roomName);
+        return "success";
     }
 }
